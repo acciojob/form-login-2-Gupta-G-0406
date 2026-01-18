01682415516825907224deaa5d95b5463085677b
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 17:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function showAlert(event) {
+      event.preventDefault(); // prevent form reload
+
+      let firstName = document.getElementById("fname").value;
+      let lastName = document.getElementById("lname").value;
+      let phone = document.getElementById("phone").value;
+      let email = document.getElementById("email").value;
+
+      alert(
+        "First Name: " + firstName + " " +
+        "Last Name: " + lastName + " " +
+        "Phone Number: " + phone + " " +
+        "Email ID: " + email
+      );
+    }
